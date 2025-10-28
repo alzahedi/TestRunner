019302d09e806d0ddcb1f0e3e374bae855c650f6
alzahedi <alzahedi@microsoft.com>
2025-10-28 19:57:18 +0530

    Added scheduler project

--- build.sh
+++ build.sh
@@ -4,15 +4,17 @@ Write-Host "Setting up build environment..."
 python -m venv venv
 .\venv\Scripts\Activate.ps1
 
-Write-Host "Installing Scheduler package"
+Write-Host "Building Scheduler package"
 # Install Scheduler package
 pip install .\scheduler-0.1.2-py3-none-any.whl
 
 Write-Host "Installing pyinstaller"
+
 # Install pyinstaller
 pip install pyinstaller
 
 Write-Host "Installing other dependencies"
+
 # Install other dependencies
 pip install -r requirements.txt
 
